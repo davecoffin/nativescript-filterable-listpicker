8221ee3af06fc4843f5ee6018cd81db8ef544c5c
Dave Coffin <dave@nimbit.com>
2018-02-22 10:21:56 -0500

    2.0 update!
    - Now you can use an array of objects in the list picker to display a
    title, image and a description.
    - Added support for webpack

--- src/filterable-listpicker.ts
+++ src/filterable-listpicker.ts
@@ -11,9 +11,8 @@ import { isIOS } from "tns-core-modules/platform";
 import * as enums from "tns-core-modules/ui/enums";
 let builder = require('tns-core-modules/ui/builder');
 
-let unfilteredSource: Array<string> = [];
+let unfilteredSource: Array<any> = [];
 let filtering: boolean = false;
-
 export const listWidthProperty = new Property<FilterableListpicker, string>({ name: "listWidth", defaultValue: '300' });
 export const listHeightProperty = new Property<FilterableListpicker, string>({ name: "listHeight", defaultValue: '300' });
 export const dimmerColorProperty = new Property<FilterableListpicker, string>({ name: "dimmerColor", defaultValue: 'rgba(0,0,0,0.8)' });
@@ -21,7 +20,7 @@ export const blurProperty = new Property<FilterableListpicker, string>({ name: "
 export const focusOnShowProperty = new Property<FilterableListpicker, boolean>({ name: "focusOnShow", defaultValue: false });
 export const hideFilterProperty = new Property<FilterableListpicker, boolean>({ name: "hideFilter", defaultValue: false });
 export const hintTextProperty = new Property<FilterableListpicker, string>({ name: "hintText", defaultValue: 'Enter text to filter...' });
-export const sourceProperty = new Property<FilterableListpicker, ObservableArray<string>>({ name: "source", defaultValue: undefined, affectsLayout: true, valueChanged: (target, oldValue, newValue) => {
+export const sourceProperty = new Property<FilterableListpicker, ObservableArray<any>>({ name: "source", defaultValue: undefined, affectsLayout: true, valueChanged: (target, oldValue, newValue) => {
     if (!filtering) {
         while (unfilteredSource.length) unfilteredSource.pop();
         newValue.forEach(element => {
@@ -33,14 +32,45 @@ export const sourceProperty = new Property<FilterableListpicker, ObservableArray
 export class FilterableListpicker extends GridLayout {
     constructor() {
         super();
-        let innerComponent = builder.load(__dirname + '/filterable-listpicker.xml') as View;
+        //let innerComponent = builder.load(__dirname + '/filterable-listpicker.xml') as View;
+        let innerComponent;
+        innerComponent = builder.parse(`
+            <GridLayout id="dc_flp_container" visibility="collapsed">
+                <StackLayout tap="{{cancel}}" width="100%" height="100%"></StackLayout>
+                <GridLayout width="{{listWidth}}" verticalAlignment="middle" rows="40, auto, 40" id="dc_flp" style="border-radius: 10;">
+                    <TextField hint="{{hintText}}" row="0" text="{{filterText}}" id="filterTextField" style="padding: 10 15; height: 40; background-color: #E0E0E0; border-radius: 10 10 0 0;"></TextField>
+                    <ListView items="{{ source }}" row="1" height="{{listHeight}}" itemTap="{{choose}}" style="background-color: white;">
+                        <ListView.itemTemplate>
+                            <StackLayout>
+                                <GridLayout columns="auto, *" visibility="{{title ? 'visible' : 'collapsed'}}">
+                                    <Image src="{{image ? image : ''}}" width="30" visibility="{{image ? 'visible' : 'collapsed'}}" stretch="aspectFit" rowSpan="2" style="margin: 10 0 10 5;"></Image>
+                                    <StackLayout style="margin: 10 10 10 5;" col="1" verticalAlignment="middle">
+                                        <Label text="{{title}}" textWrap="true" style="font-weight: bold; font-size: 16;"></Label>
+                                        <Label text="{{description ? description : ''}}" textWrap="true" visibility="{{description ? 'visible' : 'collapsed'}}" style="color: gray; font-size: 13;"></Label>
+                                    </StackLayout>
+                                </GridLayout>
+                                <Label text="{{$value}}" textWrap="true" style="margin-left: 15; padding: 10 0;" visibility="{{title ? 'collapsed' : 'visible'}}"></Label>
+                            </StackLayout>
+                        </ListView.itemTemplate>
+                    </ListView>
+                    <StackLayout row="2" height="40" style="background-color: #E0E0E0; height: 40; border-radius: 0 0 10 10;">
+                        <Button text="Cancel" tap="{{cancel}}" verticalAlignment="middle" style="font-weight: bold; height: 40; background-color: transparent; background-color: transparent; border-color: transparent; border-width: 1; font-size: 12;"></Button>    
+                    </StackLayout>
+                </GridLayout>
+            </GridLayout>`
+        );
         innerComponent.bindingContext = this;
         this.addChild(innerComponent);
         let textfield: TextField = <TextField>innerComponent.getViewById('filterTextField')
         textfield.on('textChange', (data: any) => {
             filtering = true;
             this.source = unfilteredSource.filter(item => {
-                return item.toLowerCase().indexOf(data.value.toLowerCase()) !== -1;
+                if (item.title) {
+                    return item.title.toLowerCase().indexOf(data.value.toLowerCase()) !== -1;
+                } else {
+                    return item.toLowerCase().indexOf(data.value.toLowerCase()) !== -1;
+                }
+                
             })
             filtering = false;
         })
